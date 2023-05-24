fb242cc01d521bbcb2a5564de2f6f24075930b0e
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2023-05-24 12:47:47 +0500

    добавили приложение cart + функции добаления, удаления,  подсчет  товаров в корзине + fix .gitignore

--- online_shop_project/config/settings.py
+++ online_shop_project/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CART_SESSION_ID = 'cart'
